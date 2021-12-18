4f427ac8ff047f780e12177754f36af10478243c
RomanKorotaev <kro1986@ukr.net>
2021-12-18 13:55:22 +0200

    test commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'template message!!!' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'template message!!!' })
 })
 
 router.post('/', async (req, res, next) => {
